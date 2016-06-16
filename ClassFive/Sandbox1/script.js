37f573c197b71a9deacd29dcc788127cc80bb81a
Curt Newcomer <newcomercurt@gmail.com>
2016-06-16 13:22:10 -0700

    Added sql homework. Almost finished class 5/6 snadboxing

--- ClassFive/Sandbox1/script.js
+++ ClassFive/Sandbox1/script.js
@@ -1,4 +1,15 @@
-// Sandbox for test
-$(document).ready(function() {
-  
-});
+var x = {
+  market: 'hot',
+  style: 'modern',
+  sell: function() {
+    console.log('sell now in Portland, this market is' + this.market);
+  }
+};
+console.log(x);
+var houseJson = JSON.stringify(x);
+console.log(houseJson);
+
+var newHhouseJson = houseJson.replace('market','sellers-market');
+console.log(newHhouseJson);
+var y = JSON.parse(newHhouseJson);
+console.log(y);
--- ClassFive/Sandbox3/script.js
+++ ClassFive/Sandbox3/script.js
@@ -1,4 +1,37 @@
-// Sandbox for test
-$(document).ready(function() {
-  
+// a. Use a constructor and a loop to create an array of objects, then stringify the entire array and print
+// the string to the console or DOM. Copy-and-paste the entire JSON string from that 'print out' into a new file
+// 'myData.json' that you save on your laptop.
+// var homes = [
+//   ['bungalow','hot'],
+//   ['modern','super'],
+//   ['mid-century','awesome']
+// ];
+var home = function(style, market) {
+  this.style = style;
+  this.market = market;
+  this.realtor = function () {
+    console.log('sell sell sell');
+  };
+};
+// var newHomes = [];
+// for (var i = 0; i < homes.length; i++) {
+//   newHomes.push(new home(homes[i][0] , homes[i][1]));
+// };
+//
+// console.log(newHomes);
+//
+// var newerHomes = JSON.stringify(newHomes);
+//console.log(newerHomes);
+// Use jQuery's .getJSON() to load myData.json, and use .done()
+// to set up a function F to run when the data is ready; F should call a function 'P' to print the contents of
+// the entire array of objects using a loop or .map().-->
+var newestHomes = [];
+$.getJSON('data.json', function(data) {
+  $.each(data, function(key, value) {
+    newestHomes.push(this);
+  });
+console.log(newestHomes);
+  // .done(function() {
+  //   console.log('calling f');
+  // });
 });
